812e9216f1dbd5fdd26093ebdb243e892143f54b
ariesTnT <ngocthach4471@gmail.com>
2023-07-20 08:02:41 +0700

    up

---
+++ TNTRestaurant-API/Models/BaseResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TNTRestaurant_API.Models
+{
+    public class BaseResponseModel
+    {
+        public string Status { get; set; }
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
